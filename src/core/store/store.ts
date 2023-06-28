6687bbabcefe88125499ec96fc81e00100138c1e
Sofia Hudson <sofia-sama@hotmail.com>
2023-06-28 11:54:14 +0200

    Add store

---
+++ src/core/store/store.ts
@@ -0,0 +1,17 @@
+import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
+import usersSlice from "../../features/redux/users.slice";
+
+export const store = configureStore({
+  reducer: {
+    users: usersSlice,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;

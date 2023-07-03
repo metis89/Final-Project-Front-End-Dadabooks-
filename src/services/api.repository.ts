ee533a1558336f3fafc937c03cf4b79910a2efca
Sofia Hudson <sofia-sama@hotmail.com>
2023-07-03 17:16:18 +0200

    Add functions to api.repo

--- src/services/api.repository.ts
+++ src/services/api.repository.ts
@@ -1,7 +1,7 @@
 import { SyntheticEvent } from "react";
 
 export class ApiRepository<T extends { id: string | number }> {
-  constructor(public url: string) {}
+  constructor(public url: string, public token: string) {}
 
   async getAll(url: string) {
     const response = await fetch(url);
@@ -15,6 +15,42 @@ export class ApiRepository<T extends { id: string | number }> {
   async get(id: T["id"]): Promise<T> {
     const response = await fetch(this.url + (id as string));
     return response.json() as Promise<T>;
+  }
+
+  async create(item: Partial<T>): Promise<T> {
+    const response = await fetch(this.url, {
+      method: "POST",
+      body: JSON.stringify(item),
+      headers: {
+        Authorization: "Bearer " + this.token,
+        "Content-Type": "application/json",
+      },
+    });
+    return response.json() as Promise<T>;
+  }
+
+  async update(id: T["id"], item: Partial<T>): Promise<T> {
+    const response = await fetch(this.url + (id as string), {
+      method: "PATCH",
+      body: JSON.stringify(item),
+      headers: {
+        Authorization: "Bearer " + this.token,
+        "Content-Type": "application/json",
+      },
+    });
+    return response.json() as Promise<T>;
+  }
+
+  async delete(id: T["id"]): Promise<boolean> {
+    const response = await fetch(this.url + (id as string), {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + this.token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id }),
+    });
+    return response.ok;
   }
 
   async getFiltered(filter: string) {

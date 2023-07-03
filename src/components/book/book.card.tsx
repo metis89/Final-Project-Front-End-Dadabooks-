491dfd09da156a3342a08050076344f8e68fb22f
Sofia Hudson <sofia-sama@hotmail.com>
2023-07-03 11:48:38 +0200

    Add book card

---
+++ src/components/book/book.card.tsx
@@ -0,0 +1,23 @@
+import { Book } from "../../models/book";
+import { Link } from "react-router-dom";
+
+type PropsType = {
+  item: Book;
+};
+
+export function BookCard({ item }: PropsType) {
+  return (
+    <Link to={"/details/" + item.id}>
+      <li className="book">
+        <img
+          src={item.image}
+          alt={item.title}
+          width="140"
+          height="250"
+          className="book-image"
+        />
+        <p className={item.title}> {item.title}</p>
+      </li>
+    </Link>
+  );
+}

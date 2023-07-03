48b7be6920a581cc7a1a5bbc5384ed9ddd7e4caa
Sofia Hudson <sofia-sama@hotmail.com>
2023-07-03 17:42:08 +0200

    Add book slice

---
+++ src/redux/books.slice.ts
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Book } from "../models/book";
+
+export type BookState = {
+  books: Book[];
+};
+
+const initialState: BookState = {
+  books: [],
+};
+
+const booksSlice = createSlice({
+  initialState,
+  name: "books",
+  reducers: {
+    load: (state, action) => {
+      state.books = action.payload;
+    },
+
+    add: (state, action) => {
+      state.books = action.payload;
+    },
+
+    edit: (state, action) => {
+      state.books = action.payload;
+    },
+
+    delete: (state, action) => {
+      state.books = action.payload;
+    },
+  },
+});
+
+export const { load } = booksSlice.actions;
+export default booksSlice.reducer;

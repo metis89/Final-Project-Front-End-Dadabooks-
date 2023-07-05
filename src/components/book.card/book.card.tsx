c70fc13f2311e9f6f8b0e48db5d4c9beb9ecb78f
Sofia Hudson <sofia-sama@hotmail.com>
2023-07-05 15:08:34 +0200

    Add book card

--- src/components/book.card/book.card.tsx
+++ src/components/book.card/book.card.tsx
@@ -1,5 +1,5 @@
-import { Book } from "../../models/book";
 import { Link } from "react-router-dom";
+import { Book } from "../../models/book";
 
 type PropsType = {
   item: Book;
@@ -7,13 +7,10 @@ type PropsType = {
 
 export function BookCard({ item }: PropsType) {
   return (
-    <Link to={"/details/" + item.id}>
-      <li className="books-list">
-        <p className={item.title}>{item.title}</p>
-        <p className={item.author}>{item.author}</p>
-        {/* <p className={item.year}>{item.year}</p> */}
-        <p className={item.genre}>{item.genre}</p>
-        <p className={item.synopsis}>{item.synopsis}</p>
+    <Link to={"/detail/" + item.id}>
+      <li key={item.id}>
+        <img src={item.image.url} alt={item.title} width="150" height="250" />
+        <span>{item.title}</span>
       </li>
     </Link>
   );

93e2710c39b9f31e34cfbfebdef8a17786f8f8b3
Sofia Hudson <sofia-sama@hotmail.com>
2023-07-05 15:11:17 +0200

    Add book form changes

---
+++ src/components/book.form/bookForm.tsx
@@ -0,0 +1,61 @@
+import { SyntheticEvent } from "react";
+import { url } from "../../config";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+
+export default function BookForm() {
+  const navigate = useNavigate();
+
+  const handleNewBook = async (event: SyntheticEvent) => {
+    const formRegisterElement: HTMLFormElement =
+      event.target as HTMLFormElement;
+
+    event.preventDefault();
+
+    const data = new FormData(formRegisterElement);
+
+    const urlRegister = url + "book/create";
+    const response = await fetch(urlRegister, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("store")}`,
+      },
+      body: data,
+    });
+
+    console.log(data);
+
+    const state = await response.json();
+    console.log(state);
+
+    if (state.error) {
+      Swal.fire({
+        icon: "error",
+        text: `${state.error}`,
+      });
+    } else {
+      Swal.fire({
+        icon: "success",
+        text: "Succesfully Registered!",
+      });
+    }
+
+    state.bookData = state.book;
+    delete state.book;
+    navigate("/main");
+  };
+
+  return (
+    <form className="book-form" id="form">
+      <h2 className="title_form">Please register a new book</h2>
+      <input type="text" placeholder="Title" name="title"></input>
+      <input type="text" placeholder="Author" name="author"></input>
+      <input type="text" placeholder="Year" name="year"></input>
+      <input type="text" placeholder="Synopsis" name="synopsis"></input>
+      <input type="file" placeholder="Image" name="image"></input>
+      <button type="submit" className="login_button">
+        SUBMIT
+      </button>
+    </form>
+  );
+}
